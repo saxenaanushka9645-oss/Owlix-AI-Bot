ef3486cb2bb65b4d2950ce9c6f0684138d96f4a6
Anushka Saxena <Anushka.gghs@gmail.com>
2026-04-05 21:31:21 +0530

    fix: replace sentence-transformers with chromadb ONNX embeddings, remove torch

--- chain.py
+++ chain.py
@@ -30,8 +30,7 @@ from datetime import datetime, timezone
 from dotenv import load_dotenv
 
 import requests
-from groq import Groq
-from sentence_transformers import SentenceTransformer
+
 from langchain_chroma import Chroma
 from langchain_community.utilities import SerpAPIWrapper
 
@@ -210,10 +209,17 @@ class STEmbeddingFunction(EmbeddingFunction):
         return vecs.tolist()
 
 
+from chromadb.utils.embedding_functions import ONNXMiniLM_L6_V2
+
 class STLangChainEmbeddings:
-    def __init__(self, model_name: str = HF_EMBEDDING_MODEL):
-        self.model_name = model_name
-        self.model = None
+    def __init__(self, model_name: str = None):
+        self._fn = ONNXMiniLM_L6_V2()
+
+    def embed_documents(self, texts: list[str]) -> list[list[float]]:
+        return self._fn(texts)
+
+    def embed_query(self, text: str) -> list[float]:
+        return self._fn([text])[0]
 
     def _get_model(self):
         if self.model is None:
